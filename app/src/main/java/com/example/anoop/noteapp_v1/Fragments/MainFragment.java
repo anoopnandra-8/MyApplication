63e539afca01d4478eab252ecb2bb15a70d0ab32
anoop <anoopnandra.work@gmail.com>
2016-11-17 20:17:21 +0000

    Added AlertDialogue

--- app/src/main/java/com/example/anoop/noteapp_v1/Fragments/MainFragment.java
+++ app/src/main/java/com/example/anoop/noteapp_v1/Fragments/MainFragment.java
@@ -12,9 +12,11 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.anoop.noteapp_v1.activities.NotesActivity;
 import com.example.anoop.noteapp_v1.R;
+import com.example.anoop.noteapp_v1.session.SessionManager;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,14 +30,13 @@ public class MainFragment extends Fragment {
 
     private LayoutInflater inflater;
 
+    private SessionManager session;
+    
     @BindView(R.id.mainFragImg_button)
     ImageButton addNoteFrag_imgButton;
 
     @BindView(R.id.mainFrag_textView)
     TextView addNoteFrag_txtView;
-
-    @BindView(R.id.test_btn)
-    Button alertDialogue_btn;
 
     public MainFragment(){}
 
@@ -47,28 +48,38 @@ public class MainFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        session = new SessionManager(getContext());
+        checkUserLogin();
+
+    }
+
+    public void checkUserLogin(){
+        if(!session.isLoggedIn()){
+
+            inflater = MainFragment.this.getActivity().getLayoutInflater();
+            View content = inflater.inflate(R.layout.progress_dialogue, null);
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity()); //context?
+            builder.setView(content)
+                    .setTitle("Test")
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //check to see if email has already been saved in teh db
+                        }
+                    });
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+    }
+
     @OnClick(R.id.mainFragImg_button)
     public void addNoteFragment(){
         Intent intent = new Intent(getActivity(), NotesActivity.class);
         startActivity(intent);
     }
 
-    @OnClick(R.id.test_btn)
-    public void alertDialogue_btn(){
-        inflater = MainFragment.this.getActivity().getLayoutInflater();
-        View content = inflater.inflate(R.layout.progress_dialogue, null);
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity()); //context?
-        builder.setView(content)
-                .setTitle("Test")
-                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                });
-
-        AlertDialog dialog = builder.create();
-        dialog.show();
-
-    }
 }
