4c7e9c0616bcfadef00e11e122791d0c7d45304f
anoop <anoopnandra.work@gmail.com>
2016-11-20 17:03:19 +0000

    Added Notes model

--- app/src/main/java/com/example/anoop/noteapp_v1/Fragments/MainFragment.java
+++ app/src/main/java/com/example/anoop/noteapp_v1/Fragments/MainFragment.java
@@ -1,26 +1,29 @@
 package com.example.anoop.noteapp_v1.fragments;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.anoop.noteapp_v1.activities.NotesActivity;
 import com.example.anoop.noteapp_v1.R;
+import com.example.anoop.noteapp_v1.realm.RealmUserController;
 import com.example.anoop.noteapp_v1.session.SessionManager;
+
+import java.util.HashMap;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.realm.Realm;
+import io.realm.RealmChangeListener;
 
 /**
  * Created by anoop on 10/10/2016.
@@ -29,14 +32,20 @@ import butterknife.OnClick;
 public class MainFragment extends Fragment {
 
     private LayoutInflater inflater;
-
-    private SessionManager session;
+    private SessionManager sessionManager;
+    private Realm realm;
+    private RealmChangeListener realmChangeListener;
+    private String EMAIl, NAME;
     
     @BindView(R.id.mainFragImg_button)
     ImageButton addNoteFrag_imgButton;
 
     @BindView(R.id.mainFrag_textView)
     TextView addNoteFrag_txtView;
+/*
+    @BindView(R.id.clear_btn)
+    Button clearBtn;
+*/
 
     public MainFragment(){}
 
@@ -45,34 +54,36 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         ButterKnife.bind(this, view);
+
         return view;
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        session = new SessionManager(getContext());
-        checkUserLogin();
+        this.sessionManager = new SessionManager(getActivity());
+        this.realm = RealmUserController.with(getActivity()).getRealm();
+        realmChangeListener = new RealmChangeListener() {
+            @Override
+            public void onChange(Object element) {
+                RealmUserController.with(getActivity()).refresh();
+            }
+        };
 
+        checkUserLogin();
     }
 
-    public void checkUserLogin(){
-        if(!session.isLoggedIn()){
+    private void checkUserLogin(){
+        if(sessionManager.checkLogin()==true){
+            HashMap<String, String> user  = sessionManager.getUserDetails();
+            String EMAIl = user.get(sessionManager.KEY_EMAIL);
+            String LOGNAME = user.get(sessionManager.KEY_NAME);
 
-            inflater = MainFragment.this.getActivity().getLayoutInflater();
-            View content = inflater.inflate(R.layout.progress_dialogue, null);
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity()); //context?
-            builder.setView(content)
-                    .setTitle("Test")
-                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            //check to see if email has already been saved in teh db
-                        }
-                    });
+            Log.i("DBSAVED", RealmUserController.getInstance().getUsers().toString());
 
-            AlertDialog dialog = builder.create();
-            dialog.show();
+            Log.i("CHECK_NAME", LOGNAME);
+            Log.i("CHECK_EMAIL", EMAIl);
+
         }
     }
 
@@ -81,5 +92,12 @@ public class MainFragment extends Fragment {
         Intent intent = new Intent(getActivity(), NotesActivity.class);
         startActivity(intent);
     }
-
+/*
+    @OnClick(R.id.clear_btn)
+    public void clearDB(){
+        session.clearSession();
+        RealmUserController.getInstance().clearAllUserData();
+        checkUserLogin();
+        Toast.makeText(getActivity(), String.valueOf(RealmUserController.getInstance().getDBSize()), Toast.LENGTH_SHORT).show();
+    }*/
 }
---
+++ app/src/main/java/com/example/anoop/noteapp_v1/models/Note.java
@@ -0,0 +1,120 @@
+package com.example.anoop.noteapp_v1.models;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by anoop on 20/11/2016.
+ */
+
+public class Note extends RealmObject {
+    @PrimaryKey
+    private long noteID;
+    private String title;
+    private String note;
+    private String time;
+    private String date;
+    private long emailID;
+    private Boolean archive;
+    private  Boolean delete;
+    private String colour;
+    private Boolean lock;
+    private String lockPassword;
+    private Boolean reminder;
+
+    public void setNoteID(long noteID){
+        this.noteID=noteID;
+    }
+
+    public long getNoteID(){
+        return noteID;
+    }
+
+    public void setTitle(String title){
+        this.title=title;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setNote(String note){
+        this.note=note;
+    }
+
+    public String getNote(){
+        return note;
+    }
+
+    public void setTime(String time){
+        this.time=time;
+    }
+
+    public String getTime(){
+        return time;
+    }
+
+    public void setDate(String date){
+        this.date=date;
+    }
+
+    public String getDate(){
+        return date;
+    }
+
+    public void setEmailID(long emailID){
+        this.emailID=emailID;
+    }
+
+    public long getEmailID(){
+        return emailID;
+    }
+
+    public void setArchive(Boolean archive){
+        this.archive=archive;
+    }
+
+    public Boolean getArchive(){
+        return archive;
+    }
+
+    public void setDelete(Boolean delete){
+        this.delete=delete;
+    }
+
+    public Boolean getDelete(){
+        return delete;
+    }
+
+    public void setColour(String colour){
+        this.colour=colour;
+    }
+
+    public String getColour(){
+        return colour;
+    }
+
+    public void setLock(Boolean lock){
+        this.lock=lock;
+    }
+
+    public Boolean getLock(){
+        return lock;
+    }
+
+    public void setLockPassword(String lockPassword){
+        this.lockPassword=lockPassword;
+    }
+
+    public String getLockPassword(){
+        return lockPassword;
+    }
+
+    public void setReminder(Boolean reminder){
+        this.reminder=reminder;
+    }
+
+    public Boolean getReminder(){
+        return reminder;
+    }
+}
